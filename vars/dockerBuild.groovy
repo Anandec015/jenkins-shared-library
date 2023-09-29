f99a7c4b9ab022a26acd63aa0f3674fe54d7896a
Anandec015 <114596054+Anandec015@users.noreply.github.com>
2023-09-29 14:34:34 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,8 +1,8 @@
 
 def call(String project, String hubUser) {
     sh "docker image build -t ${hubUser}/${project} ."
-    sh "docker tag ${hubUser}/${project}:${ImageTag}"
-    //sh "docker tag ${hubUser}/${project}:latest"
+    sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
+    sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
     withCredentials([usernamePassword(
             credentialsId: "docker_cred",
             usernameVariable: "USER",
